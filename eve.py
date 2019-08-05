361e4399cbec91a3477e348083f9d68edb0b48a3
Suika <2320837+Suika@users.noreply.github.com>
2019-08-05 11:08:57 +0200

    images > image, asagi compliant
    small typo, big difference

--- eve.py
+++ eve.py
@@ -355,7 +355,7 @@ class MediaFetcher(object):
 
         #make directories
         subdirs = (filename[:4], filename[4:6])
-        destinationFolder = "{}/{}/{}/{}".format(config.imageDir+"/"+self.board, "thumb" if isPreview else "images", *subdirs) #FIXME use os.path.join
+        destinationFolder = "{}/{}/{}/{}".format(config.imageDir+"/"+self.board, "thumb" if isPreview else "image", *subdirs) #FIXME use os.path.join
         os.makedirs(destinationFolder, exist_ok = True) #TODO maybe just skip this and use os.renames at the end?
 
         #set perms on directories
