4dc2c6c6bf6ba929b9b29ae3d58d84b96a6bc58d
revell <dev@kieranrevell.co.uk>
2019-10-16 22:52:23 +0100

    Made cleanup.py testable and added a test
    Allowed cleanup function to have a path specified for testing purposes,
    and added a simple test using unittest.

--- cleanup.py
+++ cleanup.py
@@ -3,7 +3,15 @@ import os
 import config
 
 #Remove truncated temporary files from downloads interrupted at the end of past runs
-for root, _, files in os.walk(config.imageDir):
-    for name in files:
-        if name[-4:] == '_tmp':
-            os.unlink(os.path.join(root, name))
+def cleanup():
+    cleanup(config.imageDir)
+
+
+def cleanup(path):
+    for root, _, files in os.walk(path):
+        for name in files:
+            if name[-4:] == "_tmp":
+                os.unlink(os.path.join(root, name))
+
+if __name__ == "__main__":
+    cleanup()
